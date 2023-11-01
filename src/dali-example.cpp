ed5758cbc9cd4048568ab1412ff35c8f926bfdc0
ANZ1217 <chihun.jeong@samsung.com>
2023-11-01 16:15:37 +0900

    add ecore loop

--- src/dali-example.cpp
+++ src/dali-example.cpp
@@ -31,10 +31,13 @@
 
 #include <thread>
 
+#include <Ecore.h>
+#include <Eldbus.h>
 #include <tbm_surface.h>
 #include <tbm_surface_internal.h>
 #include <tbm_surface_queue.h>
 
+#include <sys/eventfd.h>
 #include <unistd.h>
 
 using namespace Dali;
@@ -51,7 +54,11 @@ namespace
   const int IMAGE_HEIGHT = 256;
 
   std::thread gOffscreenThread;
+  std::thread gMainInternalThread;
 }
+
+constexpr float APP_SECONDS_PER_FRAME          = 0.033f; ///< 30.0fps
+constexpr float APP_TOTAL_RUNNING_TIME_SECONDS = 30.0f;
 // unnamed namespace
 class Example : public ConnectionTracker
 {
@@ -98,10 +105,65 @@ static void makeOffscreenApplication()
   offscreenApplication.MainLoop();
 }
 
+int gQuitFileDescriptor;
+int gTimerFileDescriptor;
+
+Eina_Bool AppTickTimer(void* data)
+{
+  fprintf(stderr, "AppTickTimer\n");
+
+  uint64_t buffer = 1;
+  fprintf(stderr, "timer fd[%d] write!\n", gTimerFileDescriptor);
+  size_t size = write(gTimerFileDescriptor, &buffer, sizeof(uint64_t));
+  if(size != sizeof(uint64_t))
+  {
+    fprintf(stderr, "timer fd[%d] write error!\n", gTimerFileDescriptor);
+  }
+
+  return EINA_TRUE;
+}
+Eina_Bool AppTerminateTimer(void* data)
+{
+  fprintf(stderr, "AppTerminateTimer\n");
+
+  uint64_t buffer = 1;
+  fprintf(stderr, "quit fd[%d] write!\n", gQuitFileDescriptor);
+  size_t size = write(gQuitFileDescriptor, &buffer, sizeof(uint64_t));
+  if(size != sizeof(uint64_t))
+  {
+    fprintf(stderr, "quit fd[%d] write error!\n", gQuitFileDescriptor);
+  }
+
+  gOffscreenThread.join();
+  ecore_main_loop_quit();
+  fprintf(stderr, "ecore_main_loop_quit\n");
+
+  return EINA_FALSE;
+}
+
+static void main_internal()
+{
+  gQuitFileDescriptor = eventfd(0, EFD_NONBLOCK);
+  gTimerFileDescriptor = eventfd(0, EFD_NONBLOCK);
+
+  ecore_init();
+
+  ecore_timer_add(APP_SECONDS_PER_FRAME, &AppTickTimer, nullptr);
+  ecore_timer_add(APP_TOTAL_RUNNING_TIME_SECONDS, &AppTerminateTimer, nullptr);
+
+  fprintf(stderr, "begin ecore_main_loop\n");
+  ecore_main_loop_begin();
+  fprintf(stderr, "end ecore_main_loop\n");
+}
+
 int DALI_EXPORT_API
 main(int argc, char **argv)
 {
   fprintf(stderr, "Main\n");
+
+  gMainInternalThread = std::thread(main_internal);
+
+  sleep(1);
 
   fprintf(stderr, "Thread 1 Start");
 
